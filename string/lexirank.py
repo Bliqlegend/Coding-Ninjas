d6510d46b1ce8f9a34f0a679612e295bb6ef31d7
Tanush Yadav <tanushkumar244@gmail.com>
2022-02-03 10:54:27 +0530

    Curing cancer with every commit

---
+++ string/lexirank.py
@@ -0,0 +1,22 @@
+def permutation(s,on,output):
+    if len(s) == 0:
+        output.append(on)
+        return 
+
+    for i in range(len(s)):
+        ch = s[i]
+        left = s[0:i]
+        right = s[i+1:]
+        rest = left + right
+        permutation(rest,on + ch,output)
+    return output
+
+def lexiRank(s):
+    sw = ''.join(sorted(s))
+    output = permutation(sw,"",[])
+    n = output.index(s)+1
+    return n
+
+s = "string"
+n = lexiRank(s)
+print(n)

3e99ca4a15348e552f172ad784992395da0e16f5
Tanush Yadav <tanushkumar244@gmail.com>
2022-02-11 22:52:11 +0530

    Update ccheckAB.py

--- recursion-2/ccheckAB.py
+++ recursion-2/ccheckAB.py
@@ -1,2 +1,13 @@
+# TRASH AS OF NOW
+
+def checkABHelper(s):
+    if len(s) == 1:
+        return s
+    if s[0] == 'a' and s[1:3]!='a':
+        return False 
+
 def checkAB(s):
-    if len(s) == 0:
+    if s[0] != 'a':
+        return False
+    checkABHelper(s)
+
